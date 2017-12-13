d4f1b504358eef98050f00da8cd22cb3f30b9458
Jong Wook Kim <jongwook@nyu.edu>
2017-12-12 19:31:04 -0500

    simpler lstm

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import data
 def train():
     train, validation, test = data.load()
 
-    model = models.spectrogram_lstm(mel=True)
+    model = models.spectrogram_lstm()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model.summary()
 
